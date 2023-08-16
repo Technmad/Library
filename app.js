daa56811db2dc53d14531ad93dcc2d8d2efc0f65
anurag <anuragpathakoffl@gmail.com>
2023-08-17 03:53:44 +0530

    basic design

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 let myLibrary = [];
+let newBook;
 
 function Book(title, author, pages, read) {
   (this.title = title),
     (this.author = author),
     (this.pages = pages),
     (this.readingStatus = read),
-    addBookToLibrary(this);
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readingStatus}  `;
-  };
+    // addBookToLibrary(this);
+    (this.info = function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readingStatus}  `;
+    });
 }
 
-function addBookToLibrary(bookObj) {
-  myLibrary.push(bookObj);
+function addBookToLibrary() {
+  preventDefault();
+  popUpForm.style.display = "none";
+  newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }
 
 const thinkAndGrowRich = new Book(
@@ -22,3 +26,14 @@ const thinkAndGrowRich = new Book(
   "Not read"
 );
 //console.log(thinkAndGrowRich.info());
+
+const submitBtn = document.querySelector("#submitBtn");
+submitBtn.addEventListener("click", addBookToLibrary);
+
+const newBookBtn = document.querySelector("#new-book");
+newBookBtn.addEventListener("click", popupDisplay);
+
+const popUpForm = document.getElementById("popup");
+
+const closePopUp = document.getElementById("close-popUp");
+closePopUp.addEventListener("click", popupDisplay);
